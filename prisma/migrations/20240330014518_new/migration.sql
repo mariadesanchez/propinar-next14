15992778ff387ef55c3ab9225a40e3af4bca884b
Lucky <calder07@gmail.com>
2024-03-29 22:46:21 -0300

    update migrate

---
+++ prisma/migrations/20240330014518_new/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+    "isPaid" BOOLEAN NOT NULL DEFAULT false,
+    "paidAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
