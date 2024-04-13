e5483d6b986f399b1f8de58feb034d3445863ab7
Lucky <calder07@gmail.com>
2024-04-13 10:52:17 -0300

    QR Url

--- src/app/(shop)/checkout/(checkout)/ui/PlaceOrder.tsx
+++ src/app/(shop)/checkout/(checkout)/ui/PlaceOrder.tsx
@@ -48,7 +48,7 @@ export const PlaceOrder = () => {
     <div className="flex justify-center mx-5 my-[-70px]">
   <div className="bg-white rounded-xl max-w-screen-lg w-full md:max-w-[800px] shadow-xl p-7 mt-20">
     <div className="text-center">
-      <QRCodeGenerator defaultUrl="https://propinar-argentina.vercel.app"
+      <QRCodeGenerator defaultUrl="https://propinar-next14.vercel.app"
        size={60} />
       <Title title="PropinAr" className="inline-block " />
     </div>
