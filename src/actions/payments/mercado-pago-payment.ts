510c6a0695fb7cd4f94cd2e073033b114534e717
Lucky <calder07@gmail.com>
2024-04-26 00:14:39 -0300

    TRIM()

--- src/actions/payments/mercado-pago-payment.ts
+++ src/actions/payments/mercado-pago-payment.ts
@@ -14,7 +14,7 @@ export const mercadoPagoCheckPayment = async (order: Order) => {
   // const accessToken = process.env['42aa5279-a23d-455a-99a4-d6c090ea8be4']!;
   // const accessToken = process.env[order.orderUserId];
    // Modificar order.orderUserId según tus especificaciones
-   const modifiedUserId = order.orderUserId.replace(/-/g, '_').replace(/^\d+/, '').toLowerCase();
+   const modifiedUserId = order.orderUserId.replace(/-/g, '_').replace(/^\d+/, '').trim().toLowerCase();
    console.log(modifiedUserId)
    // Acceder a la variable de entorno modificada
    const accessToken = process.env[modifiedUserId];
@@ -39,22 +39,22 @@ export const mercadoPagoCheckPayment = async (order: Order) => {
           unit_price: order.total,
         },
       ],
-      redirect_urls: {
-        failure: `https://propinar-arg.vercel.app/orders/${order.id}`,
-        success: `https://propinar-arg.vercel.app/orders/${order.id}`,
-      },
-      back_urls: {
-        failure: `https://propinar-arg.vercel.app/orders/${order.id}`,
-        success: `https://propinar-arg.vercel.app/orders/${order.id}`,
-      },
       // redirect_urls: {
-      //   failure: `http://localhost:3005/orders/${order.id}`,
-      //   success: `http://localhost:3005/orders/${order.id}`,
+      //   failure: `https://propinar-arg.vercel.app/orders/${order.id}`,
+      //   success: `https://propinar-arg.vercel.app/orders/${order.id}`,
       // },
       // back_urls: {
-      //   failure: `http://localhost:3005/orders/${order.id}`,
-      //   success: `http://localhost:3005/orders/${order.id}`,
+      //   failure: `https://propinar-arg.vercel.app/orders/${order.id}`,
+      //   success: `https://propinar-arg.vercel.app/orders/${order.id}`,
       // },
+      redirect_urls: {
+        failure: `http://localhost:3008/orders/${order.id}`,
+        success: `http://localhost:3008/orders/${order.id}`,
+      },
+      back_urls: {
+        failure: `http://localhost:3008/orders/${order.id}`,
+        success: `http://localhost:3008/orders/${order.id}`,
+      },
       auto_return: 'approved'
     },
   });
